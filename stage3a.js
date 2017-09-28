ddec611f0272fd6d5edbbf3a7ca80b4fbaedb9f0
Alexandre PALO <a-palo@laposte.net>
2017-09-28 11:54:46 -0400

    add special case stage3 with wd

--- stage3a.js
+++ stage3a.js
@@ -16,7 +16,21 @@ import {
   X
 } from './transformations'
 
-const solverStage3a = (cube, consecutiveU = 0) => {
+/*
+        0    1   2  3   4   5   6    7  8    9  10   11
+
+  0    [00, 00, 00, 01, 02, 03, 00, 00, 00, 00, 00, 00],
+  1    [00, 00, 00, 04, 05, 06, 00, 00, 00, 00, 00, 00],
+  2    [00, 00, 00, 07, 08, 09, 00, 00, 00, 00, 00, 00],
+  3    [10, 11, 12, 19, 20, 21, 28, 29, 30, 37, 38, 39],
+  4    [13, 14, 15, 22, 23, 24, 31, 32, 33, 40, 41, 42],
+  5    [16, 17, 18, 25, 26, 27, 34, 35, 36, 43, 44, 45],
+  6    [00, 00, 00, 46, 47, 48, 00, 00, 00, 00, 00, 00],
+  7    [00, 00, 00, 49, 50, 51, 00, 00, 00, 00, 00, 00],
+  8    [00, 00, 00, 52, 53, 54, 00, 00, 00, 00, 00, 00]
+  */
+
+const solverStage3a = (cube, consecutiveU = 0, wd = 0) => {
   let stage3CompleteA = false
 
   // First check if stage3 is needed
@@ -38,8 +52,12 @@ const solverStage3a = (cube, consecutiveU = 0) => {
         // U and retry
         return solverStage3a(U(cube), consecutiveU + 1)
       } else {
-        console.log(cube)
-        console.log('error stage 3a')
+        if (wd < 2) {
+          return solverStage3a(s3formulaA(cube), 0, wd + 1)
+        } else {
+          console.log(cube)
+          console.log('error stage 3a')
+        }
       }
     }
   } else {
